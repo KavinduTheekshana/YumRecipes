9c928ca8232f976e5194741378e0505d2498ca07
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2020-04-03 19:21:57 +0530

    Create New View

--- app/src/main/java/com/example/yum/Account.java
+++ app/src/main/java/com/example/yum/Account.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -21,6 +23,10 @@ import static com.example.yum.R.id.bottom_navigation_view;
 public class Account extends Fragment{
     BottomNavigationView account_navigation_view;
 
+    private LinearLayout profile_view;
+    private LinearLayout profile_edit_view;
+    private ImageView profile_edite;
+
     public Account() {
         // Required empty public constructor
     }
@@ -29,8 +35,26 @@ public class Account extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_account, container, false);
+
+        profile_view = v.findViewById(R.id.profile_layout);
+        profile_edite = v.findViewById(R.id.btn_go_profilr_rdite);
+        profile_edit_view = v.findViewById(R.id.profile_edite_layout);
+
+        profile_edit_view.setVisibility(View.GONE);
+
+
+        profile_edite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profile_view.setVisibility(View.GONE);
+                profile_edit_view.setVisibility(View.VISIBLE);
+            }
+        });
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return v;
 
 
     }
